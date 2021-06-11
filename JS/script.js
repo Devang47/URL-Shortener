28188573eaf27912c35666e4d87c3f8f5a373300
devang <devangsaklani@gmail.com>
2021-06-11 09:58:24 +0530

    api not working

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+let result
+
+
 AOS.init({
   duration: 300,
 });
@@ -11,14 +14,12 @@ const navbarToggle = () => {
       targets: navbar,
       height: [0, 378],
       duration: 800,
-      // easing: 'easeInOutCirc'
     });
   } else {
     anime({
       targets: navbar,
       height: [378, 0],
       duration: 800,
-      // easing: 'easeInQuad',
       changeComplete: () => {
         navbar.style.display = "";
       },
@@ -26,29 +27,12 @@ const navbarToggle = () => {
   }
 };
 
-var logotxt = document.querySelector("#footer-logo");
-logotxt.innerHTML = logotxt.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
 
-document.addEventListener("aos:in:super-duper", () => {
-  let anime1 = anime.timeline({});
-  anime1.add({
-    targets: ".letter",
-    opacity: [0, 1],
-    easing: "easeInOutQuad",
-    duration: 1000,
-    fontWeight: 700,
-    delay: (el, i) => 150 * (i + 1),
-  });
-});
-
-const getStarted = () => {
+const getStarted = (btn) => {
   let Temp = anime.timeline({});
-  let btn = document.getElementById("get-started");
+  // let btn = document.getElementById("get-started");
   Temp.add({
-    targets: "#get-started",
+    targets: btn,
     backgroundColor: "#2E294E",
     innerHTML: ["0", "100%"],
     duration: 3000,
@@ -57,7 +41,7 @@ const getStarted = () => {
     endDelay: 200,
   }).add(
     {
-      targets: "#get-started",
+      targets: btn,
       backgroundColor: "#2374AB",
       duration: 1,
       complete: () => {
@@ -66,33 +50,60 @@ const getStarted = () => {
       },
     },
     "+=200"
-  );
-};
+    );
+  };
+  
+  const shorten = () => {
+    let errorCanvas = document.querySelector("#error-msg");
+    let btnTxt = document.getElementById("shorten-btn-txt");
+    let spinner = document.getElementById("spinner");
+    let shortenBtn = document.querySelector(".shorten-btn");
+    
+    
+    let websiteLink = document.querySelector("#linkForShortening").value;
+    
+    if (websiteLink) {
+      
+      spinner.style.display = "flex";
+      btnTxt.style.display = "none";
+      shortenBtn.style.backgroundColor = "#F7CE5B";
+      
+      
+      fetch(`https://api.shrtco.de/v2/shorten?url=${websiteLink}`)
+      .then((e) => e.json())
+      .then((data) => {
+        
+        errorCanvas.style.display = "none";
+        spinner.style.display = "none";
+        btnTxt.style.display = "block";
+        shortenBtn.style.backgroundColor = "#7692FF";
+        shortenBtn.disabled = true;
+        btnTxt.innerHTML = "Done!";
+        document.querySelector("#linkForShortening").value = '';
+        result = data.result.full_short_link;
+        console.log(result);
+      })
+      .catch((res) => {
+        spinner.style.display = "none";
+        btnTxt.style.display = "block";
+        shortenBtn.style.backgroundColor = "#EF626C";
+        btnTxt.innerHTML = "ERROR!";
+        errorCanvas.style.display = 'block';
+        errorCanvas.innerHTML = res;
+        console.log(res);
+      
+    });
+  }else if(websiteLink == '')
+  {
+    
+    shortenBtn.style.backgroundColor = "#EF626C";
+    btnTxt.innerHTML = "No URL!";
+  }
+}
 
-const shorten = () => {
-  let btnTxt = document.getElementById("shorten-btn-txt");
-  let spinner = document.getElementById("spinner");
-  let shortenBtn = document.querySelector(".shorten-btn");
 
-  spinner.style.display = "flex";
-  btnTxt.style.display = "none";
-  shortenBtn.style.backgroundColor = "#F7CE5B";
-  setTimeout(() => {
-    spinner.style.display = "none";
-    btnTxt.style.display = "block";
 
-    let error = false;
-    if (error) {
-      shortenBtn.style.backgroundColor = "#7692FF";
-      shortenBtn.disabled = true;
-      btnTxt.innerHTML = "Done!";
-    } else {
-      shortenBtn.style.backgroundColor = "#EF626C";
-      // shortenBtn.disabled = true
-      btnTxt.innerHTML = "ERROR!";
-    }
-  }, 2000);
-};
+
 
 const copyLink = (ind, tag) => {
   anime({
@@ -104,19 +115,29 @@ const copyLink = (ind, tag) => {
       tag.innerHTML = "Done!";
     },
     complete: () => {
-      setTimeout(() => {
-        tag.innerHTML = "Copy";
-        tag.style.backgroundColor = "hsl(180, 66%, 49%)";
-      }, 1000);
-    },
-  });
-  // setTimeout(() => {
-  //   anime({
-  //     targets : tag,
-  //     backgroundColor = 'hsl(180, 66%, 49%)',
-  //     duration: 100,
-  //     easing: 'linear',
+        setTimeout(() => {
+          tag.innerHTML = "Copy";
+          tag.style.backgroundColor = "hsl(180, 66%, 49%)";
+        }, 1000);
+      },
+    });
+  };
 
-  //   })
-  // }, 2000);
-};
+
+  //footer logo
+  var logotxt = document.querySelector("#footer-logo");
+  logotxt.innerHTML = logotxt.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+  );
+  document.addEventListener("aos:in:super-duper", () => {
+    let anime1 = anime.timeline({});
+    anime1.add({
+      targets: ".letter",
+      opacity: [0, 1],
+      easing: "easeInOutQuad",
+      duration: 1000,
+      fontWeight: 700,
+      delay: (el, i) => 150 * (i + 1),
+    });
+  });
